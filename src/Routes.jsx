f494cf18aed53c246a7450fbaa473b84d17157cd
Chamodh Dasanayake <78280086+Cham-88D@users.noreply.github.com>
2021-08-26 03:41:06 +0530

    added new routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,9 +8,10 @@ import Footer from "./components/Footer/Footer";
 import Login from "./components/Login/Login";
 import Reg from "./components/Register/Register";
 import Prof from "./components/Profile/Profile"
-
-
-
+import Sidebar from "./components/SideBar/Sidebar";
+import Product from "./pages/userList/UserList";
+import Feed from './components/FeedBacks/Feed';
+import AddFeed from './components/AddFeed/AddFeed';
 const RouterMain = ({ component: Component, ...rest }) => {
 
   return (
@@ -27,12 +28,49 @@ const RouterMain = ({ component: Component, ...rest }) => {
   );
 };
 
+const RouterDash = ({ component: Component, ...rest }) => {
+
+  return (
+    <Route  render={props => (
+      <>
+
+    <div className="container">
+          <Sidebar />
+          <Component {...props} />
+      </div> 
+         
+
+      </>
+    )}
+    />
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
  const Url = () => (
   <Switch>
+    
     <RouterMain exact path={'/'} component={Home} />
     <RouterMain exact path={'/login'} component={Login} />
     <RouterMain exact path={'/register'} component={Reg} />
     <RouterMain exact path={'/profile'} component={Prof} />
+    <RouterMain exact path={'/feedBack'} component={AddFeed} />
+    <RouterMain exact path={'/profileUpdate'} component={Feed} />
+
+
+    <RouterDash exact path={'/dash'} component={Product} />
+    <RouterDash exact path={'/feedBackM'} component={Feed} />
+    
     <Redirect to={'/'} />
   </Switch>
 );
