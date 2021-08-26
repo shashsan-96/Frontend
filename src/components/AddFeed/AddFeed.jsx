f6a9b698063306257a996c069b0895ecd7670443
Chamodh Dasanayake <78280086+Cham-88D@users.noreply.github.com>
2021-08-26 16:13:46 +0530

    feed back add created

--- src/components/AddFeed/AddFeed.jsx
+++ src/components/AddFeed/AddFeed.jsx
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import user from "../../api/user.service";
 import auth from "../../api/auth.service";
+import fd from '../../api/feed.service';
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
-import FeedbackIcon from "@material-ui/icons/Feedback";
-import { Form, Col, Row, Button } from "react-bootstrap";
-import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import { useHistory } from "react-router";
+import {
+  Button,
+  TextField,
+  Card,
+  CardActions,
+  CardContent,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
 const useStyles = makeStyles({
   root: {
     maxWidth: 525,
@@ -18,7 +24,7 @@ const useStyles = makeStyles({
     marginTop: 40,
     marginBottom: 50,
     paddingBottom: 80,
-    backgroundColor: "rgb(242, 242, 242)",
+    backgroundColor: "white",
   },
 
   title: {
@@ -31,17 +37,81 @@ const useStyles = makeStyles({
   list: {
     fontSize: 18,
   },
+  left: {
+    marginLeft: 110,
+  },
+  formControl:
+  {
+    marginLeft: 110,
+  },
+  size:{
+    paddingLeft:100
+  }
 });
 
 const Feed = () => {
   const classes = useStyles();
-
+  const history = useHistory();
   useEffect(() => {
-    const id = auth.getCurrentUser().id;
-    if (id === null) {
-      window.location.href = "/";
-    }
-  }, []);
+
+    user.getUser().then(
+      (response) => {
+        const id = auth.getCurrentUser().id;
+        if (id === null) {
+          window.location.href = "/";
+        }},
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+          console.error(resMessage);
+        if (resMessage!==null ) {
+          auth.logout();
+          history.push("/"); 
+        }
+      })
+  });
+
+
+
+  let today = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = mm + "/" + dd + "/" + yyyy;
+
+  const [message, setText] = React.useState("");
+  const [rate, setRate] = React.useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    setRate(event.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    fd.createFeedBack(rate,message,today).then(
+      (response) => {
+        console.log(response+"Done")
+       },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+          console.error(resMessage);
+
+
+  });}
 
   return (
     <div>
@@ -57,19 +127,48 @@ const Feed = () => {
         </CardContent>
 
         <CardActions>
-          <Form>
-          <FloatingLabel controlId="floatingSelect" label="Works with selects">
-  <Form.Select aria-label="Floating label select example">
-    <option>Open this select menu</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </Form.Select>
-</FloatingLabel>
-            <Button variant="primary" type="submit">
+          <form onSubmit={handleSubmit}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label">Rate</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={rate}
+                onChange={handleChange2}
+                className={classes.size}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+              </Select>
+            </FormControl>
+
+            <br />
+            <br />
+
+            <TextField
+              fullWidth
+              multiline
+              label="Input Message"
+              value={message}
+              onChange={handleChange}
+              className={classes.left}
+            />
+
+            <br />
+            <br />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              startIcon={<SendIcon />}
+              className={classes.left}
+            >
               Submit
             </Button>
-          </Form>
+          </form>
         </CardActions>
       </Card>
     </div>
