0f4a50367b8f4b0bc29c01824296e5b3ac7b8a59
user <narthanamendis1997@gmail.com>
2021-10-12 12:49:30 +0530

    Committed order actions

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,5 +1,7 @@
 import Axios from 'axios';
 import { CART_EMPTY } from '../constants/cartConstants';
+
+//importing order constants
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
@@ -22,8 +24,13 @@ import {
   ORDER_DELIVER_REQUEST,
   ORDER_DELIVER_SUCCESS,
   ORDER_DELIVER_FAIL,
+
+  ORDER_UPDATE_SHIPPING_FAIL,
+  ORDER_UPDATE_SHIPPING_REQUEST,
+  ORDER_UPDATE_SHIPPING_SUCCESS,
 } from '../constants/orderConstants';
 
+//connecting with create order api action
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
   try {
@@ -49,6 +56,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
   }
 };
 
+//connecting with get details of one specific order api
 export const detailsOrder = (orderId) => async (dispatch, getState) => {
   dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
   const {
@@ -68,6 +76,7 @@ export const detailsOrder = (orderId) => async (dispatch, getState) => {
   }
 };
 
+// connecting with order payment api
 export const payOrder = (order, paymentResult) => async (
   dispatch,
   getState
@@ -89,6 +98,8 @@ export const payOrder = (order, paymentResult) => async (
     dispatch({ type: ORDER_PAY_FAIL, payload: message });
   }
 };
+
+//connecting with order history api
 export const listOrderMine = () => async (dispatch, getState) => {
   dispatch({ type: ORDER_MINE_LIST_REQUEST });
   const {
@@ -109,6 +120,8 @@ export const listOrderMine = () => async (dispatch, getState) => {
     dispatch({ type: ORDER_MINE_LIST_FAIL, payload: message });
   }
 };
+
+//connecting with all orders of all users api
 export const listOrders = () => async (dispatch, getState) => {
   dispatch({ type: ORDER_LIST_REQUEST });
   const {
@@ -128,6 +141,8 @@ export const listOrders = () => async (dispatch, getState) => {
     dispatch({ type: ORDER_LIST_FAIL, payload: message });
   }
 };
+
+//connecting with delete order api
 export const deleteOrder = (orderId) => async (dispatch, getState) => {
   dispatch({ type: ORDER_DELETE_REQUEST, payload: orderId });
   const {
@@ -147,6 +162,7 @@ export const deleteOrder = (orderId) => async (dispatch, getState) => {
   }
 };
 
+//connecting with update delivery status api
 export const deliverOrder = (orderId) => async (dispatch, getState) => {
   dispatch({ type: ORDER_DELIVER_REQUEST, payload: orderId });
   const {
@@ -169,3 +185,24 @@ export const deliverOrder = (orderId) => async (dispatch, getState) => {
     dispatch({ type: ORDER_DELIVER_FAIL, payload: message });
   }
 };
+
+//connecting with update shipping details of one specific order
+export const updateShipping = (orderId) => async (dispatch, getState) =>{
+  dispatch({ type: ORDER_UPDATE_SHIPPING_REQUEST, payload: orderId});
+  const {
+    userSignin : {userInfo},
+  } = getState();
+
+  try {
+    const { data } = await Axios.put(`/api/orders/${orderId}/edit`, {}, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: ORDER_UPDATE_SHIPPING_SUCCESS, payload: data});
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: ORDER_UPDATE_SHIPPING_FAIL, payload: message });
+  }
+}
